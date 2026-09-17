154b5529944e5c6c33464662df1883d35f7371c5
caeltlg <rooxzie@gmail.com>
2026-09-17 14:51:04 -0300

    Bind Render health-check server to 0.0.0.0

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Bot Online!'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor HTTP do bot rodando na porta ${PORT}`);
 });
 
