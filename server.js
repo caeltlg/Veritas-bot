8b7cbf6db1d15cbdecc758030c088fd0893a7456
caeltlg <rooxzie@gmail.com>
2026-09-17 15:13:16 -0300

    Fix Render port scan timeout by binding Express to 0.0.0.0

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => res.send('Bot Online!'));
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor HTTP do bot rodando na porta ${PORT}`);
+  console.log(`Servidor de health-check escutando na porta ${PORT}`);
 });
 
 require('./index');
